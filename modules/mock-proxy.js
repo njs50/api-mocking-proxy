2b1dfc18223c9ed1b838ed062d88f227ffc5e329
Colin Bate <colin@colinbate.com>
2016-01-19 12:05:46 -0400

    Cleaning up whitespace and some structure.

--- modules/mock-proxy.js
+++ modules/mock-proxy.js
@@ -4,108 +4,115 @@ var cacher = require('./response-cacher')();
 var appUtils = require('./app-utils')();
 var Q = require('q');
 
-module.exports = function() {
-	
-	return {
-        _mappings: {},
-        
-		init: function(docRoot, mappings) {	
-            this._mappings = mappings;
-			cacher.init(docRoot);
-		},
-		execute: function(req, res) {
-            var deferred = Q.defer(),
-                conf = this._conf(req);
-            
-			// There is a cached response
-			if (this._loadCached(conf, req, res)) {
-                deferred.resolve();
-                return deferred.promise;
+module.exports = function () {
+
+  return {
+    _mappings: {},
+
+    init: function (docRoot, mappings) {
+      this._mappings = mappings;
+      cacher.init(docRoot);
+    },
+    execute: function (req, res) {
+      var deferred = Q.defer(),
+          conf = this._conf(req);
+
+      // There is a cached response
+      if (this._loadCached(conf, req, res)) {
+        deferred.resolve();
+        return deferred.promise;
+      }
+
+      var handler = function(err, retRes, body) {
+        if (!err && /^2\d\d$/.test(retRes.statusCode)) {
+          var data = {
+            code: retRes.statusCode,
+            headers: retRes.headers,
+            body: body
+          };
+
+          var me = this;
+          cacher.set(conf, req, data).then(
+            function() {
+              me._success(res, data);
+              deferred.resolve();
+            },
+            function(err) {
+              me._error(res, err);
+              deferred.reject(err);
             }
-                        
-			var handler = function(err, retRes, body) {
-                if (!err && /^2\d\d$/.test(retRes.statusCode)) {
-                    var data = {
-                        code: retRes.statusCode,
-                        headers: retRes.headers,
-                        body: body
-                    };
+          );
 
-                    var me = this;
-                    cacher.set(conf, req, data).then(
-                        function() {
-                            me._success(res, data);
-                            deferred.resolve();
-                        },
-                        function(err) {
-                            me._error(res, err);
-                            deferred.reject(err);
-                        }
-                    );
-
-                } else {
-                    this._error(res, err);
-                    deferred.reject(err);
-                }
-			};
-            
-            var hostUrl = conf.host + appUtils.stripApiKey(conf.key, req.url),
-                method, urlConf;
-
-            switch (req.method) {
-                case 'GET':
-                case 'DELETE':
-                    urlConf = {url: hostUrl, timeout: timeout};
-                    method = (req.method === 'GET') ? request : request.del;
-                    break;
-                case 'POST':
-                case 'PUT':
-                    urlConf = {url: hostUrl, form: req.body, timeout: timeout};
-                    method = (req.method === 'POST') ? request.post : request.put;
-                    break;
-                default:
-                    deferred.reject(new Error('Invalid HTTP method: ' + req.method));
-            }
-            if (method) method(urlConf, handler.bind(this));
-            return deferred.promise;
-		},
-		_conf: function(req) {               
-            
-            // remove a leading slash if there is any
-            var reqUrl = req.url.startsWith('/') ? req.url.replace('/','') : req.url;   
-            
-			for (var key in this._mappings) {                
-                var host = this._mappings[key].host;
-				if (this._equals(reqUrl, key) || this._equals(reqUrl, host)) {                    
-                    var mapping = this._mappings[key];
-					return {key: key, dir: mapping.dir, host: mapping.host, skipHeaders: mapping.skipHeaders};
-				}
-			}
-			throw new Error('No configuration found!');
-		},   
-        _equals: function(value, key) {
-            return value.substring(0, key.length) === key;
-        },        
-		_loadCached: function(conf, req, res) {
-			var cached = false,
-                data = cacher.get(conf, req);
-			
-			if (data) {
-				this._success(res, JSON.parse(data));
-				cached = true;
-			}
-			return cached;
-		},
-		_success: function(res, options) {
-            res.writeHead(options.code || 200, options.headers);
-            res.write(options.body);
-            res.end();
-        },
-        _error: function(res, err) {
-            console.error('request failed: ' + err);
-            res.writeHead(500, {"Content-Type": 'text/plain'});
-            res.write('An error has occured, please review the logs.');
-            res.end();
+        } else {
+          this._error(res, err);
+          deferred.reject(err);
         }
-	};
+      };
+
+      var hostUrl = conf.host + appUtils.stripApiKey(conf.key, req.url),
+          method, urlConf;
+
+      switch (req.method) {
+        case 'GET':
+        case 'DELETE':
+          urlConf = {url: hostUrl, timeout: timeout};
+          method = (req.method === 'GET') ? request : request.del;
+          break;
+        case 'POST':
+        case 'PUT':
+          urlConf = {url: hostUrl, form: req.body, timeout: timeout};
+          method = (req.method === 'POST') ? request.post : request.put;
+          break;
+        default:
+          deferred.reject(new Error('Invalid HTTP method: ' + req.method));
+      }
+      if (method) {
+        method(urlConf, handler.bind(this));
+      }
+      return deferred.promise;
+    },
+    _conf: function(req) {
+
+      // remove a leading slash if there is any
+      var reqUrl = req.url.startsWith('/') ? req.url.replace('/','') : req.url;
+
+      for (var key in this._mappings) {
+        var host = this._mappings[key].host;
+        if (this._equals(reqUrl, key) || this._equals(reqUrl, host)) {
+          var mapping = this._mappings[key];
+          return {
+            key: key,
+            dir: mapping.dir,
+            host: mapping.host,
+            skipHeaders: mapping.skipHeaders
+          };
+        }
+      }
+      throw new Error('No configuration found!');
+    },
+    _equals: function(value, key) {
+      return value.substring(0, key.length) === key;
+    },
+    _loadCached: function(conf, req, res) {
+      var cached = false,
+          data = cacher.get(conf, req);
+
+      if (data) {
+        this._success(res, JSON.parse(data));
+        cached = true;
+      }
+      return cached;
+    },
+    _success: function(res, options) {
+      res.writeHead(options.code || 200, options.headers);
+      res.write(options.body);
+      res.end();
+    },
+    _error: function(res, err) {
+      console.error('request failed: ' + err);
+      res.writeHead(500, {"Content-Type": 'text/plain'});
+      res.write('An error has occured, please review the logs.');
+      res.end();
+    }
+  };
 };
--- modules/response-cacher.js
+++ modules/response-cacher.js
@@ -4,55 +4,66 @@ var appUtils = require('./app-utils')();
 var Q = require('q');
 
 module.exports = function() {
-    
-	return {
-        _docRoot: undefined,
-        
-		init: function(docRoot) {
-			this._docRoot = appUtils.getAbsPath(docRoot);
-		},
-		get: function(conf, req) {            
-			return this._readMock(this._resolveMockPath(conf, req));	
-		},
-		set: function(conf, req, options) {
-            if (!options) throw new Error("Invalid argument: options must be provided!");
-            var deferred = Q.defer(),
-                mockPath = this._resolveMockPath(conf, req);
-            filendir.wa(mockPath, JSON.stringify(options), function(err) {
-                if (err) deferred.reject(err);
-                else deferred.resolve();
-            });
-            return deferred.promise;
-		},
-        _resolveMockPath: function(conf, req) {
-			            
-            // Mock data directory associated with the API call
-			var path = appUtils.getDataDir(conf, req);            
-			if (!path) return null;
 
-			// Custom headers
-            if (!conf.skipHeaders) {
-                var headers = appUtils.getReqHeaders(req);
-                if (headers) path = appUtils.bind(path, headers);            
-            }
+  return {
+    _docRoot: undefined,
 
-			// Meta info regarding the request's url, including the query string
-			var parts = appUtils.getUrlParts(appUtils.stripApiKey(conf.key, req.url));
+    init: function(docRoot) {
+      this._docRoot = appUtils.getAbsPath(docRoot);
+    },
+    get: function(conf, req) {
+      return this._readMock(this._resolveMockPath(conf, req));
+    },
+    set: function(conf, req, options) {
+      if (!options) {
+        throw new Error("Invalid argument: options must be provided!");
+      }
+      var deferred = Q.defer(),
+          mockPath = this._resolveMockPath(conf, req);
+      filendir.wa(mockPath, JSON.stringify(options), function(err) {
+        if (err) {
+          deferred.reject(err);
+        } else {
+          deferred.resolve();
+        }
+      });
+      return deferred.promise;
+    },
+    _resolveMockPath: function(conf, req) {
 
-			if (parts) {
-				// REST parameters
-				var urlPath = appUtils.getUrlPath(parts);
-				if (urlPath) path = appUtils.bind(path, urlPath);
+      // Mock data directory associated with the API call
+      var path = appUtils.getDataDir(conf, req);
+      if (!path) {
+        return null;
+      }
 
-				// Query string
-                var qs = appUtils.getQueryString(urlPath, parts, req);
-				if (qs) path = (urlPath) ? path += qs : appUtils.bind(path, qs);				
-			}
-          
-            return appUtils.bind(this._docRoot, path + '.mock');
-        },
-		_readMock: function(mockPath) {            
-			return (fs.existsSync(mockPath)) ? fs.readFileSync(mockPath, 'utf8') : '';            
-		}
-	};
+      // Custom headers
+      if (!conf.skipHeaders) {
+        var headers = appUtils.getReqHeaders(req);
+        if (headers) path = appUtils.bind(path, headers);
+      }
+
+      // Meta info regarding the request's url, including the query string
+      var parts = appUtils.getUrlParts(appUtils.stripApiKey(conf.key, req.url));
+
+      if (parts) {
+        // REST parameters
+        var urlPath = appUtils.getUrlPath(parts);
+        if (urlPath) {
+          path = appUtils.bind(path, urlPath);
+        }
+
+        // Query string
+        var qs = appUtils.getQueryString(urlPath, parts, req);
+        if (qs) {
+          path = (urlPath) ? path += qs : appUtils.bind(path, qs);
+        }
+      }
+
+      return appUtils.bind(this._docRoot, path + '.mock');
+    },
+    _readMock: function(mockPath) {
+      return (fs.existsSync(mockPath)) ? fs.readFileSync(mockPath, 'utf8') : '';
+    }
+  };
 };
